4b742c6805df77f436f7ee415d5ef2d29978d601
Luan Perosa Chitto <luan.chitto@webjump.com.br>
2020-10-19 10:09:25 -0300

    Style header, footer

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */'use strict';/**
+ * Define Themes
+ *
+ * area: area, one of (frontend|adminhtml|doc),
+ * name: theme name in format Vendor/theme-name,
+ * locale: locale,
+ * files: [
+ * 'css/styles-m',
+ * 'css/styles-l'
+ * ],
+ * dsl: dynamic stylesheet language (less|sass)
+ *
+ */
+module.exports = {
+  academia: {
+      area: 'frontend',
+      name: 'Webjump/theme-frontend-academia',
+      locale: 'pt_BR',
+      files: [
+          'css/styles-m',
+          'css/styles-l'
+      ],
+      dsl: 'less'
+  }
+}
